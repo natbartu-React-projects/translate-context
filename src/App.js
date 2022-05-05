2a236ead77cb17b170865c8c1e2bd1b935e35b41
natacha <nat_bartu@hotmail.com>
2022-05-05 11:57:31 +1000

    set up state for choose language state on the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <h1>App</h1>
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag br"
+            onClick={() => this.onLanguageChange("portuguese")}
+          />
+        </div>
+        {this.state.language}
       </div>
     );
   }
