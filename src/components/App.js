629ce5fd8add66fb4a13876f346575109939d36e
natacha <nat_bartu@hotmail.com>
2022-05-09 10:14:08 +1000

    use Context to define a value for define a a render type button display"

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -22,8 +24,11 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("portuguese")}
           />
         </div>
-
-        <UserCreate />
+        <ColorContext.Provider value="primary">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
